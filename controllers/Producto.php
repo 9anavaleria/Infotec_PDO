81ce7e1ef995c5581078042bd3794caae3b746ab
9anavaleria <95309779+9anavaleria@users.noreply.github.com>
2023-07-19 20:49:40 -0500

    Update Producto.php

--- controllers/Producto.php
+++ controllers/Producto.php
@@ -3,8 +3,6 @@
     require_once "models/dao_model/Producto_dao.php";
     require_once "models/dto_model/Categoria_dto.php";
     require_once "models/dao_model/Categoria_dao.php";
-    
-
     class Producto{
         private $productoDao;
         private $categoriaDao;
@@ -14,66 +12,49 @@
             
         }
         public function index(){
-            $producto = $this->productoDao->readProductoDao();
-            $categoria =$this->categoriaDao->readCategoriaDao();
+            // Vista de datos 
+            $verProducto = $this->productoDao->readProductoDao();
+            $verCategoria = $this->categoriaDao->verCategoriaDao();
+            $alerta = '';
             if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id_categoria'])){
                 $result =$this->productoDao->consultarProductoDao($_GET['id_categoria']);
                 $Producto_dto=new Producto_dto($result[0],$result[1]);
                 $Producto_dto->setIdCategoria($result[0]);
             }
             elseif($_SERVER['REQUEST_METHOD'] == 'POST'){
-                // Capturar datos
-                $Producto_dto=new Producto_dto($_POST['id_categoria'],$_POST['id_producto'],$_POST['nombre_producto'],$_POST['precio_producto'],$_POST['exist_producto']);
-                
-                $this->productoDao->createProducto($Producto_dto);
-                header("Loaction: ?c=Producto");
+                if(!empty($_POST['id_categoria']) && !empty($_POST['id_producto']) && !empty($_POST['nombre_producto']) && !empty($_POST['precio_producto']) && !empty($_POST['exist_producto'])){
+                    // Capturar datos
+                    $producto_dto=new Producto_dto($_POST['id_categoria'],$_POST['id_producto'],$_POST['nombre_producto'],$_POST['precio_producto'],$_POST['exist_producto']);
+                    
+                    $this->productoDao->createProducto($producto_dto);
+                    header("Loaction: ?c=Producto");
+                }
+                else{
+                    $alerta ='Existen campos vacios';
+                }
             }
             require_once "views/roles/admin/header_dash.php";
             require_once "views/modules/2_products/2_2producto/producto.view.php";
             require_once "views/roles/admin/footer.php";   
         }
-        public function crear_producto(){
-            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-                require_once "views/roles/admin/header_dash.php";
-                require_once "views/modules/2_products/2_2producto/producto.view.php";
-                require_once "views/roles/admin/footer.php";  
-        }
-        elseif($_SERVER['REQUEST_METHOD'] == 'POST'){
-            $Producto_dto=new Producto_dto($_POST['id_categoria'],$_POST['id_producto'],$_POST['nombre_producto'],$_POST['precio_producto'],$_POST['exist_producto']);
-            $this->productoDao->createProducto($Producto_dto);
-            header("Location: ?c=Producto&a=crear_producto");
-            } else{
-                header("Location: ?c=Producto&a=crear_producto");
-            }
-        }
-        public function eliminar_producto(){
-            if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-                    
-                // Capturar Datos
-                $this->productoDao->eliminarProductoDao($_GET['id_producto']); 
-            }
-            $producto = $this->productoDao->readProductoDao();
-            $categoria =$this->categoriaDao->readCategoriaDao();
-            require_once "views/roles/admin/header_dash.php";
-            require_once "views/modules/2_products/2_2producto/producto.view.php";
-            require_once "views/roles/admin/footer.php";
-        }
         public function editar_producto(){
             if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-                $editpro=$this->productoDao->actualizarProductoDao($_GET['id_producto']);
-                $categoria =$this->categoriaDao->readCategoriaDao();
-                
+                $producto = $this->productoDao->actualizarProductoDao($_GET['id_producto'])[0];
+                $categoria =$this->categoriaDao->verCategoriaDao();
             }
             require_once "views/roles/admin/header_dash.php";
             require_once "views/modules/2_products/2_2producto/producto.editar.php";
             require_once "views/roles/admin/footer.php";
         }
         public function modificar_producto(){
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                
-                $this->productoDao->modificarProductoDao($_POST['id_categoria'],$_POST['id_producto'],$_POST['nombre_producto'],$_POST['precio_producto'],$_POST['exist_producto'],);
-                header("Location:?c=Producto");
-            }
+            $producto_dto = new Producto_dto ($_POST['id_categoria'],$_POST['id_producto'],$_POST['nombre_producto'],$_POST['precio_producto'],$_POST['exist_producto'])
+            $this->produdtoDao->modificarProductoDao($producto_dto);
+            header("Loaction: ?c=Producto");
         }
-    }
+        public function eliminar_producto(){
+            $this->productoDao->eliminarProductoDao($_GET['id_producto']); 
+            header("Loaction: ?c=Producto");
+        }
+           
+    }    
 ?>
