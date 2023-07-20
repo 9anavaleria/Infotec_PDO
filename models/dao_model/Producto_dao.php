c92981f52cd088c1b426ff38f8a2281769595fc7
9anavaleria <95309779+9anavaleria@users.noreply.github.com>
2023-07-19 19:31:13 -0500

    Update Producto_dao.php

--- models/dao_model/Producto_dao.php
+++ models/dao_model/Producto_dao.php
@@ -1,82 +1,63 @@
 <?php
     class Producto_dao{
         private $pdo;
-        public function __construct(){
+        ppublic function __construct(){
 			try {
-				$this->pdo = Database::connection();				
+				$db = new DataBase();
+                $this->pdo = $db->connection();	
+                
 			} catch (Exception $e) {
 				die($e->getMessage());
 			}
-            
-		}	
-       public function createProducto ($producto_dto){
-        try {
-            $sql='INSERT INTO productos VALUES(
-                '.$producto_dto->getIdCategoria().',
-                "'.$producto_dto->getIdProducto().'",
-                
-                "'.$producto_dto->getNombreProducto().'",
-                '.$producto_dto->getPrecioProducto().',
-                '.$producto_dto->getExistProducto().'
-                )';
-                
-                mysqli_query($this->pdo,$sql);
-                
-			} catch (Exception $e) {
-				die("....".$e->getMessage());
+		}
+        public function verProductoDao(){
+            $sql="SELECT p.id_categoria,p.id_producto,p.nombre_producto,p.precio_producto,p.exist_producto,c.id_categoria,c.nombre_categoria FROM productos p inner join categoria c on p.id_categoria=c.id_categoria;";
+            $resultado = $this->pdo->query($sql);
+			$verProducto = $resultado->fetchAll();
+			return $verProducto;
         }
-       } 
-       public function readProductoDao(){
-        try {
-            $sql='SELECT p.id_categoria,p.id_producto,p.nombre_producto,p.precio_producto,p.exist_producto,c.id_categoria,c.nombre_categoria FROM productos p inner join categoria c on p.id_categoria=c.id_categoria ';
-        
-			$dbh = mysqli_query($this->pdo,$sql);
-            return $dbh;
-            } catch (Exception $e) {
-            die($e->getMessage());
-            }
-       }
-       public function consultarProductoDao($idprodu){
-        try {
-            $sql= 'SELECT * FROM productos WHERE id_producto='.$idprodu.'';
+        public function consultarProductoDao($idProducto){
+            $sql= "SELECT * FROM productos WHERE id_producto='$idProducto'";
             $dbh = mysqli_query($this->pdo,$sql);
             return mysqli_fetch_row($dbh);
-        } catch (Exception $e) {
-            die($e->getMessage());
+            $resultado = $this->pdo->query($sql);
+			$consulta = $resultado->fetchAll();
+            return $consulta;
         }
-       }
-       public function eliminarProductoDao($elimpro){
+        public function crearProductoDao(){
+            try{
+                $sql = "INSERT INTO producto (`id_categoria`, `id_producto`,`nombre_producto`,`precio_producto`,`exist_producto`) values (?, ?, ?, ?, ?)"
+                $resultado = $this->pdo->prepare($sql);
+                $resultado->execute(array($producto_dto->getIdCategoria(),$producto_dto->getIdProducto(),$producto_dto->getNombreProducto(),$producto_dto->getPrecioProducto(),$producto_dto->getExistProducto()));
+            	return $resultado->rowCount();
+			} 
+            catch (Exception $e) {
+				die("....".$e->getMessage());	
+			}
+        }
+        public function modificarProductoDao($producto_dto){
+            try {
+                $sql = "UPDATE productos SET id_categoria=?,nombre_producto=?,precio_producto=?,
+                exist_producto=? WHERE id_producto=?";
+                $resultado->execute(array($producto_dto->getIdCategoria(),$producto_dto->getNombreProducto(),$producto_dto->getPrecioProducto(),$producto_dto->getExistProducto(),$producto_dto->getIdProducto()));
+                return $resultado->rowCount();
+            } 
+            catch (Exception $e) {
+				die("....".$e->getMessage());	
+			}
+        }
+        public function eliminarProductoDao($productoid){
         try{
-            $sql='DELETE FROM productos WHERE id_producto='.strval($elimpro);
+            $sql = "DELETE FROM productos WHERE id_producto=$productoid";
             $dbh = mysqli_query($this->pdo,$sql);
-            return $dbh;
-           
-        } catch (Exception $e) {
-            die($e->getMessage());
+            $resultado = $this->pdo->prepare($sql);
+            $resultado->execute(array($rolid));
+            return $resultado->rowCount();
+        }
+        catch (Exception $e) {
+            die("....".$e->getMessage());	
         }
        }
-       public function actualizarProductoDao($actua){
-        try{
-            $sql ='SELECT * FROM productos WHERE id_producto="' .$actua.'"';
-            $dbh = mysqli_query($this->pdo,$sql);
-            
-				return mysqli_fetch_row($dbh);
-					
-			} catch (Exception $e) {
-				die($e->getMessage());
-        }
-       }
-       public function modificarProductoDao($idCategoria,$idProducto,$nombreProducto,$precioProducto,$existProducto){
-        try {
-            $sql= "UPDATE productos SET id_categoria='$idCategoria',nombre_producto='$nombreProducto',precio_producto='$precioProducto',
-            exist_producto='$existProducto' WHERE id_producto= '$idProducto'";
-          
-            $dbh = mysqli_query($this->pdo,$sql);
-			return $dbh;
-		    } catch (Exception $e) {
-			die($e->getMessage());
-		    }
-        }
     }
 
 ?>
