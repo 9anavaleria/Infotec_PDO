192818e28c8b74ed4e516be1895a368cd960bc4a
9anavaleria <95309779+9anavaleria@users.noreply.github.com>
2023-08-01 21:02:26 -0500

    Update Servicio.php

--- controllers/Servicio.php
+++ controllers/Servicio.php
@@ -4,47 +4,46 @@
     class Servicio{
         private $servicioDao;
         public function __construct(){
-            $this->servicioDao=new Servicios_dao;
+            $this->ServicioDao = new Servicio_dao;
         }
         public function index(){
-            $servicios=$this->servicioDao->readServicioDao();
+            $verServicios = $this->ServicioDao->verServicioDao();
+            $alerta = '';
             if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id_servicios'])){
                 $result=$this->servicioDao->consultarServicioDao($_GET['id_servicios']);
-                $servicio_dto=new Servicios_dto($result[0],$result[1]);
+                $servicio_dto=new Servicio_dto($result[0],$result[1]);
                 $servicio_dto->setIdServicio($result[0]);
             }
             elseif($_SERVER['REQUEST_METHOD'] == 'POST'){
-                $servicio_dto=new Servicios_dto($_POST['id_servicios'],$_POST['nombre_servicio'],$_POST['precio_servicio']);
+                if( && !empty($_POST['id_servicios']) && !empty($_POST['nombre_servicio']) && !empty($_POST['precio_servicio']))
+                {
+                $servicio_dto=new Servicio_dto($_POST['id_servicios'],$_POST['nombre_servicio'],$_POST['precio_servicio']);
                 
-                $this->servicioDao->createServicio($servicio_dto);
+                $this->servicioDao->crearServicio($servicio_dto);
                 header("Location: ?c=Servicio");
+                }else{
+                    $alerta = 'Existen campos vacios';
+                }
             }
-            require_once "views/roles/admin/header_dash.php";
-            require_once "views/modules/2_products/2_3servicio/servicio.view.php";
-            require_once "views/roles/admin/footer.php";
-        }
-        public function eliminar_servicio(){ 
-            if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-                $this->servicioDao->eliminarServicioDao($_GET['id_servicios']);
-            }
-            $servicios=$this->servicioDao->readServicioDao();
             require_once "views/roles/admin/header_dash.php";
             require_once "views/modules/2_products/2_3servicio/servicio.view.php";
             require_once "views/roles/admin/footer.php";
         }
         public function editar_servicio(){
-            if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-                $edtiser=$this->servicioDao->actualizarServicioDao($_GET['id_servicios']);
-            }
+            $servicio_dto = $this->servicioDao->consultarServicioDao($_GET['id_servicios'])[0];
             require_once "views/roles/admin/header_dash.php";
             require_once "views/modules/2_products/2_3servicio/servicio.editar.php";
             require_once "views/roles/admin/footer.php";
         }
         public function modificar_servicio(){
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                $this->servicioDao->modificarServicioDao($_POST['id_servicios'],$_POST['nombre_servicio'],$_POST['precio_servicio']);
-                header("Location: ?c=Servicio");
-            }
+            $servicio_dto = new Servicio_dto ($_POST['id_servicios'],$_POST['nombre_servicio'],$_POST['precio_servicio']);
+            $this->servicioDao->modificar_servicio($servicio_dto);
+            header("Location: ?c=Servicio");
         }
-    }
+        
+        public function eliminar_servicio(){ 
+            $this->servicioDao->eliminarServicioDao($_GET['id_servicios']);
+            header("Location: ?c=Servicio");
+        }
+}
 ?>
